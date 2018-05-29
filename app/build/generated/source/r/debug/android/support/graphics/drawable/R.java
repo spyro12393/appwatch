df4acf7585162645b199f4992f6b6871d33ed144
Poyen Tseng <pytseng@g.ncu.edu.tw>
2018-05-29 22:13:23 +0800

    add icon & debug

--- app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
+++ app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
@@ -50,18 +50,18 @@ public final class R {
         public static final int notification_top_pad_large_text = 0x7f060094;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f070065;
-        public static final int notification_bg = 0x7f070066;
-        public static final int notification_bg_low = 0x7f070067;
-        public static final int notification_bg_low_normal = 0x7f070068;
-        public static final int notification_bg_low_pressed = 0x7f070069;
-        public static final int notification_bg_normal = 0x7f07006a;
-        public static final int notification_bg_normal_pressed = 0x7f07006b;
-        public static final int notification_icon_background = 0x7f07006c;
-        public static final int notification_template_icon_bg = 0x7f07006d;
-        public static final int notification_template_icon_low_bg = 0x7f07006e;
-        public static final int notification_tile_bg = 0x7f07006f;
-        public static final int notify_panel_notification_icon_bg = 0x7f070070;
+        public static final int notification_action_background = 0x7f070066;
+        public static final int notification_bg = 0x7f070067;
+        public static final int notification_bg_low = 0x7f070068;
+        public static final int notification_bg_low_normal = 0x7f070069;
+        public static final int notification_bg_low_pressed = 0x7f07006a;
+        public static final int notification_bg_normal = 0x7f07006b;
+        public static final int notification_bg_normal_pressed = 0x7f07006c;
+        public static final int notification_icon_background = 0x7f07006d;
+        public static final int notification_template_icon_bg = 0x7f07006e;
+        public static final int notification_template_icon_low_bg = 0x7f07006f;
+        public static final int notification_tile_bg = 0x7f070070;
+        public static final int notify_panel_notification_icon_bg = 0x7f070071;
     }
     public static final class id {
         public static final int action_container = 0x7f08000e;
@@ -76,19 +76,19 @@ public final class R {
         public static final int icon = 0x7f080051;
         public static final int icon_group = 0x7f080052;
         public static final int info = 0x7f080056;
-        public static final int italic = 0x7f080058;
-        public static final int line1 = 0x7f08005c;
-        public static final int line3 = 0x7f08005d;
-        public static final int normal = 0x7f08006a;
-        public static final int notification_background = 0x7f08006b;
-        public static final int notification_main_column = 0x7f08006c;
-        public static final int notification_main_column_container = 0x7f08006d;
-        public static final int right_icon = 0x7f08007b;
-        public static final int right_side = 0x7f08007c;
-        public static final int text = 0x7f0800aa;
-        public static final int text2 = 0x7f0800ab;
-        public static final int time = 0x7f0800b3;
-        public static final int title = 0x7f0800b4;
+        public static final int italic = 0x7f080059;
+        public static final int line1 = 0x7f08005d;
+        public static final int line3 = 0x7f08005e;
+        public static final int normal = 0x7f08006b;
+        public static final int notification_background = 0x7f08006c;
+        public static final int notification_main_column = 0x7f08006d;
+        public static final int notification_main_column_container = 0x7f08006e;
+        public static final int right_icon = 0x7f08007c;
+        public static final int right_side = 0x7f08007d;
+        public static final int text = 0x7f0800ab;
+        public static final int text2 = 0x7f0800ac;
+        public static final int time = 0x7f0800b5;
+        public static final int title = 0x7f0800b6;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f090009;
--- app/src/main/java/com/yan/appwatch/DetailsActivity.java
+++ app/src/main/java/com/yan/appwatch/DetailsActivity.java
@@ -1,5 +1,6 @@
 package com.yan.appwatch;
 
+import android.app.ActivityManager;
 import android.app.AppOpsManager;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
@@ -7,25 +8,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.TimeZone;
 
 public class DetailsActivity extends AppCompatActivity {
     private PackageManager packageManager = null;
     private ListView lv;
+    private TextView tv_usedTime,isrunning_tv;
     private AppListAdapter adapter;
+    private long usedTime;
+    private Date d;
+    private SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");//format sec to HH:mm:ss
     ArrayList<AppList> arrayList ;
     ApplicationInfo appinfo;
 
@@ -33,7 +45,9 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        isrunning_tv = findViewById(R.id.isrunning_tv);
         lv = findViewById(R.id.listView);
+        tv_usedTime = findViewById(R.id.tv_UsedTime);
         arrayList = new ArrayList<>();
         packageManager = this.getPackageManager();
         openPermission();
@@ -49,6 +63,17 @@ public class DetailsActivity extends AppCompatActivity {
                 getApplicationContext(), R.layout.use_list_layout, arrayList
         );
         lv.setAdapter(adapter);
+        /**聽服務是否啟動*/
+        boolean isRunning = isServiceRunning(getApplicationContext(),"com.yan.appwatch.MonitorService");
+        if (isRunning) {
+            isrunning_tv.setText("服務執行中");
+            isrunning_tv.setTextColor(Color.WHITE);
+            isrunning_tv.setBackgroundColor(Color.RED);
+        } else {
+            isrunning_tv.setText("服務已關閉");
+            isrunning_tv.setTextColor(Color.WHITE);
+            isrunning_tv.setBackgroundColor(Color.rgb(0,180,30));
+        }
     }
 
     public void onBackPressed() {
@@ -100,23 +125,48 @@ public class DetailsActivity extends AppCompatActivity {
 
         if (usageStatsList != null && !usageStatsList.isEmpty()) {
             HashSet<String> set = new HashSet<>();
+            usedTime=0;
+            d= new Date(usedTime);
             for (UsageStats usageStats : usageStatsList) {
                 set.add(usageStats.getPackageName());
+                d.setTime(usageStats.getTotalTimeInForeground());
                 try {
                     appinfo = packageManager.getApplicationInfo(usageStats.getPackageName(), 0);
                     arrayList.add(new AppList(
                             appinfo.loadIcon(packageManager),
                             appinfo.loadLabel(packageManager).toString(),
                             usageStats.getPackageName(),
-                            String.valueOf(usageStats.getTotalTimeInForeground() / 1000+"秒")
+                            String.valueOf(df.format(d))
                     ));
-
+                    usedTime += usageStats.getTotalTimeInForeground();//加總今日使用時間
                 } catch (Exception e) {}
             }
+            /**總時間*/
+            d.setTime(usedTime);
+            tv_usedTime.setText(df.format(d));
+
             if (!set.isEmpty()) {
                 Log.e("size", set.size() + "");
             }
         }
 
+    }
+
+    private boolean isServiceRunning(Context context, String serviceName) {
+        if (!TextUtils.isEmpty(serviceName) && context != null) {
+            ActivityManager activityManager
+                    = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            ArrayList<ActivityManager.RunningServiceInfo> runningServiceInfoList
+                    = (ArrayList<ActivityManager.RunningServiceInfo>) activityManager.getRunningServices(100);
+            for (Iterator<ActivityManager.RunningServiceInfo> iterator = runningServiceInfoList.iterator(); iterator.hasNext();) {
+                ActivityManager.RunningServiceInfo runningServiceInfo = (ActivityManager.RunningServiceInfo) iterator.next();
+                if (serviceName.equals(runningServiceInfo.service.getClassName().toString())) {
+                    return true;
+                }
+            }
+        } else {
+            return false;
+        }
+        return false;
     }
 }
