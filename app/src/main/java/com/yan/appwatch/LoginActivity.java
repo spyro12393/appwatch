b8e7490f0deb55a3cebbcad3dc7909abbef7cf41
JJ Yeh <spyro12393@gmail.com>
2018-06-01 11:21:02 +0800

    new Login/Register feature

---
+++ app/src/main/java/com/yan/appwatch/LoginActivity.java
@@ -0,0 +1,145 @@
+package com.yan.appwatch;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    FirebaseAuth.AuthStateListener authListener;
+    private String userUID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // 輸入email, password回傳userID
+        auth = FirebaseAuth.getInstance();
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(
+                    @NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!=null) {
+                    Log.d("onAuthStateChanged", "登入:"+ user.getUid());
+                    userUID =  user.getUid();
+
+                }else{
+                    Log.d("onAuthStateChanged", "已登出");
+                }
+            }
+        };
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authListener);
+    }
+
+
+    // 註冊功能
+    private void register(final String email, final String password) {
+        final Intent intent = new Intent(this,RegisterActivity.class);
+        new AlertDialog.Builder(LoginActivity.this)
+                .setTitle("登入問題")
+                .setMessage("無此帳號，是否要以此帳號與密碼註冊?")
+                .setPositiveButton("註冊",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // createUser(email, password);
+
+                                intent.putExtra("email", email);
+                                //intent.putExtra("password", password);
+
+                                finish();
+                                startActivity(intent);
+                            }
+                        })
+                .setNeutralButton("取消", null)
+                .show();
+    }
+
+    private void createUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                String message =
+                                        task.isSuccessful() ? "註冊成功" : "註冊失敗";
+
+                                new AlertDialog.Builder(LoginActivity.this)
+                                        .setMessage(message)
+                                        .setPositiveButton("OK", null)
+                                        .show();
+
+                                // add into database
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference myRef = database.getReference("User");
+
+                                myRef.setValue(userUID);
+                            }
+                        });
+    }
+
+    // clickLogin
+    public void login(View v) {
+        try {
+            final String email = ((EditText) findViewById(R.id.email))
+                    .getText().toString();
+            final String password = ((EditText) findViewById(R.id.password))
+                    .getText().toString();
+            Log.d("AUTH", email + "/" + password);
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()){
+                                Log.d("onComplete", "登入失敗");
+                                register(email, password);
+                            }
+                        }
+                    });
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(v.getContext(), "Cannot be empty!",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void clickRegister(View view) {
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        finish();
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/yan/appwatch/RegisterActivity.java
@@ -0,0 +1,93 @@
+package com.yan.appwatch;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView mText;
+    private FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener authListener;
+    private String userUID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // String email = getIntent().getStringExtra("email");
+        // String password = getIntent().getStringExtra("password");
+
+        // mText = findViewById(R.id.txtAccount);
+        // mText.setText(email);
+
+    }
+
+    public void clickSubmit(View view) {
+
+        String account = ((EditText) findViewById(R.id.txtAccount))
+                .getText().toString();
+        String password = ((EditText) findViewById(R.id.txtPwd))
+                .getText().toString();
+        String confirmPassword = ((EditText) findViewById(R.id.txtConfirmPwd))
+                .getText().toString();
+
+        Log.d("註冊","email/pwd/confirm: " + account + "/" + password + "/" + confirmPassword);
+        if(password.equals(confirmPassword))
+        {
+            createUser(account, password);
+        }
+        else
+        {
+            Toast.makeText(view.getContext(), "確認密碼不同!",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+    private void createUser(String email, String password) {
+        Log.d("註冊","email/pwd: " + email + "/" + password);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                String message =
+                                        task.isSuccessful() ? "註冊成功" : "註冊失敗";
+
+
+                                new AlertDialog.Builder(RegisterActivity.this)
+                                        .setMessage(message)
+                                        .setPositiveButton("OK", null)
+                                        .show();
+
+
+                                Log.d("新增UID:",  userUID);
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference myRef = database.getReference("User");
+
+                                myRef.setValue(userUID);
+                            }
+                        });
+    }
+
+
+}
